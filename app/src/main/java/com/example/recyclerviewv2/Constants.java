f4c63ac156ff27c4f2a889cdf679cdb5acdbeffd
Centurion <allain@et.esiea.fr>
2020-04-20 09:51:58 +0200

    Stockage de la data en cache

---
+++ app/src/main/java/com/example/recyclerviewv2/Constants.java
@@ -0,0 +1,5 @@
+package com.example.recyclerviewv2;
+
+public class Constants {
+    static String KEY_POKEMON_LIST = "jsonPokemonList";
+}
--- app/src/main/java/com/example/recyclerviewv2/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewv2/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,26 +30,33 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private com.example.recyclerview.ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private SharedPreferences sharedPreferences;
+    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-      /*  recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
-        recyclerView.setHasFixedSize(true);
-        // use a linear layout manager
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        List<String> input = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            input.add("Test" + i);
-        }// define an adapter
-        mAdapter = new com.example.recyclerview.ListAdapter(input);
-        recyclerView.setAdapter(mAdapter);*/
-      //showList();
-      makeApiCall();
+        sharedPreferences =getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
+        gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        List<Pokemon> pokemonList = getDataFromCache();
+        if(pokemonList!=null){
+            showList(pokemonList);
+        }else {
+            makeApiCall();
+        }
+    }
+    private List<Pokemon> getDataFromCache(){
+        String jsonPokemon =sharedPreferences.getString(Constants.KEY_POKEMON_LIST,null);
+        if(jsonPokemon ==null){
+            return null;
+        }else {
+            Type listType = new TypeToken<List<Pokemon>>() {
+            }.getType();
+            return gson.fromJson(jsonPokemon, listType);
+        }
     }
 
     private void showList(List<Pokemon> pokemonList) {
@@ -66,9 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void makeApiCall(){
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
+
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -83,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<RestPokemonResponse> call, Response<RestPokemonResponse> response) {
                 if (response.isSuccessful()&& response.body() != null) {
                     List<Pokemon> pokemonList = response.body().getResults();
-                    //Toast.makeText( getApplicationContext(),"Api Success",Toast.LENGTH_SHORT).show();
+                    saveList(pokemonList);
                     showList(pokemonList);
                 }else{
 
@@ -99,6 +108,15 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    private void saveList(List<Pokemon> PokemonList){
+        String jsonString = gson.toJson(PokemonList);
+        sharedPreferences
+                .edit()
+                .putString(Constants.KEY_POKEMON_LIST,jsonString)
+                .apply();
+        Toast.makeText( this,"List Saved",Toast.LENGTH_SHORT).show();
     }
 
     private void showError() {
