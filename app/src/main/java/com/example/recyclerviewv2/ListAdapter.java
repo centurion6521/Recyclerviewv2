10455d403625e64a3ef014b49b10474a6711befe
Centurion <allain@et.esiea.fr>
2020-04-20 08:54:13 +0200

    Affichage des données reçus de la base en ligne

--- app/src/main/java/com/example/recyclerviewv2/ListAdapter.java
+++ app/src/main/java/com/example/recyclerviewv2/ListAdapter.java
@@ -8,12 +8,13 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.recyclerviewv2.Pokemon;
 import com.example.recyclerviewv2.R;
 
 import java.util.List;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
-    private List<String> values;
+    private List<Pokemon> values;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -32,7 +33,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
         }
     }
 
-    public void add(int position, String item) {
+    public void add(int position, Pokemon item) {
         values.add(position, item);
         notifyItemInserted(position);
     }
@@ -43,7 +44,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public ListAdapter(List<String> myDataset) {
+    public ListAdapter(List<Pokemon> myDataset) {
         values = myDataset;
     }
 
@@ -66,16 +67,13 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     public void onBindViewHolder(ViewHolder holder, final int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        final String name = values.get(position);
-        holder.txtHeader.setText(name);
-        holder.txtHeader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                remove(position);
-            }
-        });
+        final Pokemon currentPokemon = values.get(position);
+        holder.txtHeader.setText(currentPokemon.getName());
+       /* holder.txtHeader.setOnClickListener(new View.OnClickListener() {
 
-        holder.txtFooter.setText("Footer: " + name);
+        });*/
+
+        holder.txtFooter.setText(currentPokemon.getUrl());
     }
 
     // Return the size of your dataset (invoked by the layout manager)
---
+++ app/src/main/java/com/example/recyclerviewv2/PokeApi.java
@@ -0,0 +1,10 @@
+package com.example.recyclerviewv2;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface PokeApi {
+    @GET("/api/v2/pokemon")
+    Call<RestPokemonResponse> getPokemonResponse();
+}
